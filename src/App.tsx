c41c39a73fce68e69ccf48547991ca605522432a
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-17 19:05:27 +0300

    clone react_phone-catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Header } from './components/Header';
+import './utils/resetting.css';
+import { Outlet } from 'react-router-dom';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+      <Outlet />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import { AppProvider } from './AppContext';
+import { HashRouter as Router } from 'react-router-dom';
+import './App.scss';
+import './utils/resetting.css';
+import { Main } from './Main';
 
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './index.scss';
-
-import { App } from './App';
-
-createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <AppProvider>
+      <Main />
+    </AppProvider>
+  </Router>,
+);
